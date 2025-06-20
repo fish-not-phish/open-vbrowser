88c51b3580c9ccbc081e28349cc92f200e41e841
Joseph Fisher <joseph_n_fisher@hotmail.com>
2025-06-20 17:52:12 -0500

    terminal fix

--- docker/containers-update/vbrowsers/terminal/entrypoint.sh
+++ docker/containers-update/vbrowsers/terminal/entrypoint.sh
@@ -14,7 +14,6 @@ echo -e "\e[1;36mY88  88P      \e[1;38;2;255;20;147m\"888 \e[1;38;2;255;20;147m8
 echo -e "\e[1;36m Y8bd8P\e[1;38;2;255;20;147m Y88b  d88P \e[1;38;2;255;20;147m888  888 Y8b.     888 888     "
 echo -e "\e[1;36m  Y88P   \e[1;38;2;255;20;147m\"Y8888P\"  \e[1;38;2;255;20;147m888  888  \"Y8888  888 888     "
 echo -e "\e[0m"
-echo "Enter 'help' for a list of available commands"
 EOF
 
 echo 'export PS1="\[\e[1m\]\[\e[38;2;255;20;147m\]vUser\[\e[0m\]\[\e[1;36m\]@\[\e[0m\]\[\e[1m\]\[\e[38;2;255;20;147m\]vShell\[\e[0m\]:\[\e[1;36m\]\w\[\e[0m\]\[\e[1;33m\]\$ \[\e[0m\]"' >> /home/vUser/.bashrc
