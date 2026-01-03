7cd2d62fb6d8e1150a8787d420d8faad86978f8c
Joseph Fisher <joseph_n_fisher@hotmail.com>
2026-01-03 16:42:18 -0600

    remove and modify dockerfiles Fixes #7

---
+++ docker/containers-update/install-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+apt update -y
+apt install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update -y
+
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
